454060a128c3078f1b3d984de9f55e84cf25fb9c
Evan Weinberg <weinbe2@bu.edu>
2017-02-17 17:01:34 -0500

    Added a (probably) working staggered inverter with even/odd support.

--- operators/staggered.h
+++ operators/staggered.h
@@ -12,15 +12,18 @@ using std::complex;
 #include "lattice/lattice.h"
 #include "stencil/stencil_2d.h"
 
-// Set up a gauged laplace.
+// Pre-declare an inline function to set eta phases.
+inline void staggered_set_eta_y(int i, complex<double>& elem, void* extra_data);
+
+// Set up a gauged staggered operator.
 // This largely just requires overloading the 
-// constructor.
-struct GaugedLaplace2D : public Stencil2D
+// constructor, plus supplying a few convenience functions. 
+struct Staggered2D : public Stencil2D
 {
 protected:
   // Get rid of copy, assignment.
-  GaugedLaplace2D(GaugedLaplace2D const &);
-  GaugedLaplace2D& operator=(GaugedLaplace2D const &);
+  Staggered2D(Staggered2D const &);
+  Staggered2D& operator=(Staggered2D const &);
 
   // Temporary space for eo solve.
   complex<double>* tmp_eo_space;
@@ -28,12 +31,12 @@ protected:
 public:
 
   // Base constructor.
-  GaugedLaplace2D(Lattice2D* in_lat, complex<double> mass_sq, complex<double>* gauge_links)
-    : Stencil2D(in_lat, QMG_PIECE_CLOVER_HOPPING, mass_sq, 0.0, 0.0)
+  Staggered2D(Lattice2D* in_lat, complex<double> mass, complex<double>* gauge_links)
+    : Stencil2D(in_lat, QMG_PIECE_HOPPING, mass, 0.0, 0.0)
   {
     if (lat->get_nc() != 1)
     {
-      std::cout << "[QMG-ERROR]: GaugedLaplace2D only supports Nc = 1.\n";
+      std::cout << "[QMG-ERROR]: Stggered2D only supports Nc = 1.\n";
       return;
     }
 
@@ -41,8 +44,7 @@ public:
     tmp_eo_space = allocate_vector<complex<double>>(lat->get_size_cv());
 
     // The gauged laplace doesn't have a self-interaction term besides the
-    // mass, so it's just a 4 on the clover.
-    constant_vector(clover, 4, lat->get_size_cm());
+    // mass, so the clover term never gets allocated.
 
     // The hopping term is a bit more complicated, but not by much!
     zero_vector(hopping, lat->get_size_hopping());
@@ -52,23 +54,25 @@ public:
     // The +x, +y hopping terms are easy.
     
     // +x
-    caxpy(-1.0, gauge_links, hopping, cm_size); // Copy rescaled +x links in.
+    caxpy(-0.5, gauge_links, hopping, cm_size); // Copy rescaled +x links in.
 
-    // +y
-    caxpy(-1.0, gauge_links + cm_size, hopping + cm_size, cm_size);
+    // +y, set phases.
+    caxpy(-0.5, gauge_links + cm_size, hopping + cm_size, cm_size);
+    arb_local_function_vector(hopping + cm_size, staggered_set_eta_y, (void*)lat, cm_size);
 
     // -x requires a cshift, conj.
     cshift(priv_cmatrix, gauge_links, QMG_CSHIFT_FROM_XM1, QMG_EO_FROM_EVENODD, 1, lat);
     conj_vector(priv_cmatrix, cm_size);
-    caxpy(-1.0, priv_cmatrix, hopping + 2*cm_size, cm_size);
+    caxpy(0.5, priv_cmatrix, hopping + 2*cm_size, cm_size);
 
-    // -y requires a cshift, conj.
+    // -y requires a cshift, conj, set phases.
     cshift(priv_cmatrix, gauge_links + cm_size, QMG_CSHIFT_FROM_YM1, QMG_EO_FROM_EVENODD, 1, lat);
     conj_vector(priv_cmatrix, cm_size);
-    caxpy(-1.0, priv_cmatrix, hopping + 3*cm_size, cm_size);
+    caxpy(0.5, priv_cmatrix, hopping + 3*cm_size, cm_size);
+    arb_local_function_vector(hopping + 3*cm_size, staggered_set_eta_y, (void*)lat, cm_size);
   }
 
-  ~GaugedLaplace2D()
+  ~Staggered2D()
   {
     deallocate_vector(&tmp_eo_space);
   }
@@ -76,7 +80,7 @@ public:
   // update gauge links.
   void update_links(complex<double>* gauge_links)
   {
-    // Only need to update the hopping term.
+    // Only need to update hopping term. 
     zero_vector(hopping, lat->get_size_hopping());
 
     const int cm_size = lat->get_size_cm();
@@ -84,20 +88,22 @@ public:
     // The +x, +y hopping terms are easy.
     
     // +x
-    caxpy(-1.0, gauge_links, hopping, cm_size); // Copy rescaled +x links in.
+    caxpy(-0.5, gauge_links, hopping, cm_size); // Copy rescaled +x links in.
 
-    // +y
-    caxpy(-1.0, gauge_links + cm_size, hopping + cm_size, cm_size);
+    // +y, set phases.
+    caxpy(-0.5, gauge_links + cm_size, hopping + cm_size, cm_size);
+    arb_local_function_vector(hopping + cm_size, staggered_set_eta_y, (void*)lat, cm_size);
 
     // -x requires a cshift, conj.
     cshift(priv_cmatrix, gauge_links, QMG_CSHIFT_FROM_XM1, QMG_EO_FROM_EVENODD, 1, lat);
     conj_vector(priv_cmatrix, cm_size);
-    caxpy(-1.0, priv_cmatrix, hopping + 2*cm_size, cm_size);
+    caxpy(0.5, priv_cmatrix, hopping + 2*cm_size, cm_size);
 
-    // -y requires a cshift, conj.
+    // -y requires a cshift, conj, set phases.
     cshift(priv_cmatrix, gauge_links + cm_size, QMG_CSHIFT_FROM_YM1, QMG_EO_FROM_EVENODD, 1, lat);
     conj_vector(priv_cmatrix, cm_size);
-    caxpy(-1.0, priv_cmatrix, hopping + 3*cm_size, cm_size);
+    caxpy(0.5, priv_cmatrix, hopping + 3*cm_size, cm_size);
+    arb_local_function_vector(hopping + 3*cm_size, staggered_set_eta_y, (void*)lat, cm_size);
   }
 
   // Custom functions to prepare for eo preconditioned solve.
@@ -112,8 +118,8 @@ public:
     // Apply D_{eo}.
     apply_M_eo(b_new, b);
 
-    // Form (4 + m^2) b_e - D_{eo} b_0
-    caxpby(4.0+shift, b, complex<double>(-1.0), b_new, cv_size/2); // even only.
+    // Form shift b_e - D_{eo} b_0
+    caxpby(shift, b, complex<double>(-1.0), b_new, cv_size/2); // even only.
   }
 
   // Custom function for eo preconditioned op application.
@@ -134,12 +140,12 @@ public:
     // Apply D_{eo}
     apply_M_eo(tmp_eo_space, tmp_eo_space);
 
-    // Form ((4+m^2)^2 - D_{eo}D_{oe})
-    caxpbyz((4.0+shift)*(4.0+shift), rhs, complex<double>(-1.0), tmp_eo_space, lhs, cv_size/2);
+    // Form (m^2 - D_{eo}D_{oe})
+    caxpbyz(shift*shift, rhs, complex<double>(-1.0), tmp_eo_space, lhs, cv_size/2);
   }
 
   // Custom function to reconstruct after eo preconditioned solve.
-  // x_0 = 1/(4+m^2)[b_o - D_{oe} x_e]
+  // x_0 = 1/m[b_o - D_{oe} x_e]
   void reconstruct_x(complex<double>* x, complex<double>* b)
   {
     int cv_size = lat->get_size_cv();
@@ -150,17 +156,27 @@ public:
     // Apply D_{oe}
     apply_M_oe(x, x);
 
-    // Form 1/(4+m^2)[b_o - D_{oe} x_e]
-    caxpby(1.0/(4.0+shift), b + cv_size/2, -1.0/(4.0+shift), x + cv_size/2, cv_size/2);
+    // Form 1/m[b_o - D_{oe} x_e]
+    caxpby(1.0/shift, b + cv_size/2, -1.0/shift, x + cv_size/2, cv_size/2);
   }
 
 };
 
 // Special C function wrapper for e-o stencil application.
-void apply_eo_gauge_laplace_2D_M(complex<double>* lhs, complex<double>* rhs, void* extra_data)
+void apply_eo_staggered_2D_M(complex<double>* lhs, complex<double>* rhs, void* extra_data)
 {
-  GaugedLaplace2D* stenc = (GaugedLaplace2D*)extra_data;
+  Staggered2D* stenc = (Staggered2D*)extra_data;
   stenc->apply_eo_prec_M(lhs, rhs); // internally zeroes lhs.
+}
+
+// Special C function to set eta phases. Expects a LatticeColorMatrix. Should get
+// called twice: once for +y, once for -y.
+inline void staggered_set_eta_y(int i, complex<double>& elem, void* extra_data)
+{
+  Lattice2D* lat = (Lattice2D*)extra_data;
+  int x, y, c1, c2;
+  lat->cm_index_to_coord(i, x, y, c1, c2);
+  elem *= (double)(1.0-2.0*(x%2));
 }
 
 #endif
